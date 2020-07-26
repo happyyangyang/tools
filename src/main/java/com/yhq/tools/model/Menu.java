e3ed5e3bdfa5f73ec64d18324a2b89b3dfc339a9
yanghuiqing <yhq0319abc@163.com>
2020-07-26 11:11:08 +0800

    添加菜单controller

---
+++ src/main/java/com/yhq/tools/model/Menu.java
@@ -0,0 +1,12 @@
+package com.yhq.tools.model;
+
+/**
+ * @author : yanghuiqing
+ * @version V1.0
+ * @Project: tools
+ * @Package com.yhq.tools.model
+ * @Description: TODO
+ * @date Date : 2020年07月26日 10:48
+ */
+public class Menu {
+}
